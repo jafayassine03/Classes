f146d1a6c866da22fe416a18e470c31d0e5f5bce
Jafa Yassine <jafayassine03@gmail.com>
2026-07-29 23:16:23 +0800

    Update Class1.py

--- Class1/Class1.py
+++ Class1/Class1.py
@@ -3,45 +3,40 @@ if num % 2:
     print("your number is odd")
 else:
     print("your number is even")
-    
-# --------------------------------------------
 
-# result = int(input("what's your score?: "))
-# if result <= 59:
-#     print("E")
-# elif 60<=result<=69:
-#     print("D")
-# elif 70<=result<=79:
-#     print("C")
-# elif 80<=result<=89:
-#     print("B")
-# elif 90<=result<=100:
-#     print("A")
-# else:
-    
-#     if result > 100:
-#         print("i think your number is up to 100 :), you can repeat again...")    
-    
-# #--------------------------------------------    
+result = int(input("what's your score?: "))
+if result <= 59:
+    print("E")
+elif 60 <= result <= 69:
+    print("D")
+elif 70 <= result <= 79:
+    print("C")
+elif 80 <= result <= 89:
+    print("B")
+elif 90 <= result <= 100:
+    print("A")
+else:
+    if result > 100:
+        print("i think your number is up to 100 :), you can repeat again...")
 
-# import random
-# print("you have to guess the number between 1 to 100 :) ")
-# rand = random.randint(1, 100)
-# guesses = 0
+import random
+print("you have to guess the number between 1 to 100 :) ")
+rand = random.randint(1, 100)
+guesses = 0
 
-# while True:
-#     guesses += 1
-#     x = int(input("type your guess: "))
-#     if x < rand:
-#         print("too low!") 
-#     if x > rand:
-#         print("too high!")
-#     elif x == rand:
-#         print("your guess is correct, you guess" ,guesses ,"times") 
-#         break
-# else:
-#     print("try again...")
+while True:
+    guesses += 1
+    x = int(input("type your guess: "))
+    if x < rand:
+        print("too low!")
+    elif x > rand:
+        print("too high!")
+    else:
+        print("your guess is correct, you guess", guesses, "times")
+        break
 
 import math
 num2 = int(input("write another number for factorial: "))
 print("Factorial:", math.factorial(num2))
+
+print("Thanks for using the program!")
